9adc8670ce6732c660945b41e582565aa0014350
kartik bhatt <kartikbhatt0001@gmail.com>
2024-04-27 20:41:31 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const heading = React.createElement("h1", { id: "heading" }, "hello world");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const parent = React.createElement(
+   "div",
+   { id: "parent" },
+   React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "this is h1 tag"),
+      React.createElement("h2", {}, "this is h2 tag"),
+   ])
+);
+root.render(parent);
+
+/**
+ *
+ *
+ *
+ * <div>
+ *    div
+ *       p
+ *    div
+ * div
+ */
