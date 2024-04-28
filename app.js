ac71d81ff9c8635e910d6939e05040cd6198e48e
kartik bhatt <kartikbhatt0001@gmail.com>
2024-04-28 14:20:25 +0530

    react-components, elements, jsx

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+// react element same as dom element
+// const heading = React.createElement("h1", { id: "heading" }, "this is heading");
 
-const heading = React.createElement("h1", { id: "heading" }, "hello world");
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// jsx - html like syntax => jsx => React.createelement() => react.createlement- js object=> html element
+const heading = <h1 id="heading">hello world with jsx</h1>;
 
-const parent = React.createElement(
-   "div",
-   { id: "parent" },
-      React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "this is h1 tag"),
-      React.createElement("h2", {}, "this is h2 tag"),
-   ])
+const Title = () => <h1 className="head">Heading from components</h1>;
+
+const title = (
+   <div className="hehe">
+      <h1>title element</h1>
+   </div>
 );
-root.render(parent);
+
+// component composition
+const HeadingComponent = () => (
+   <div className="container">
+      <Title />
+      {title}
+      <h1 className="heading">heading using components</h1>
+   </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />);
