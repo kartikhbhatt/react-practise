0fa187fc7156284d8f903b04b95b011f942cc872
kartik bhatt <kartikbhatt0001@gmail.com>
2024-04-28 11:57:31 +0530

    package.json configuration

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const parent = React.createElement(
    "div",
    { id: "parent" },
-   React.createElement("div", { id: "child" }, [
+      React.createElement("div", { id: "child" }, [
       React.createElement("h1", {}, "this is h1 tag"),
       React.createElement("h2", {}, "this is h2 tag"),
    ])
